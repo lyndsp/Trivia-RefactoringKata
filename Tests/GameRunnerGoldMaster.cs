a5f957832b19b5da04434c2dc49265f55a10bd00
Lyndsay Prewer <lyndsayp@gmail.com>
2014-07-09 12:49:46 +0100

    Created the golden master output files

---
+++ Tests/GameRunnerGoldMaster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UglyTrivia
+{
+    public class GameRunnerGoldMaster
+    {
+        private bool notAWinner;
+
+        public void Run(int seed)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            Random rand = new Random(seed);
+
+            do
+            {
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+        }
+    }
+}
---
+++ Tests/GoldenMaster.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
+
+namespace UglyTrivia
+{
+    [Ignore("Regenerates the golden output")]
+    [TestFixture]
+    class GoldenMaster
+    {
+        [Test]
+        public void OutputGoldenLog()
+        {
+            var baseOutputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GoldenOutput-Seed-{0}.txt");
+
+            var seeds = new int[100];
+            var actualSeed = 0;
+
+            foreach (var i in seeds)
+            {
+                actualSeed++;
+
+                using (var output = new StringWriter())
+                {
+                    Console.SetOut(output);
+                    var runner = new GameRunnerGoldMaster();
+
+                    runner.Run(actualSeed);
+
+                    File.WriteAllText(string.Format(baseOutputPath, actualSeed), output.ToString());
+                }
+            }
+        }
+    }
+}
