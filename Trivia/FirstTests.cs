3374b2fde256e4ba28fdb6f0d8b0c12facf0cb0c
Lyndsay Prewer <lyndsayp@gmail.com>
2014-07-09 11:05:57 +0100

    Passing unit tests

---
+++ Trivia/FirstTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace UglyTrivia
+{
+    [TestFixture]
+    class FirstTests
+    {
+        [Test]
+        public void NewGameHasZeroPlayers()
+        {
+            var game = new Game();
+
+            Assert.That(game.howManyPlayers(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanAddOnePlayer()
+        {
+            var game = new Game();
+
+            game.add("player");
+
+            Assert.That(game.howManyPlayers(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CanAddMulitplePlayers()
+        {
+            var game = new Game();
+
+            game.add("player");
+            game.add("player");
+
+            Assert.That(game.howManyPlayers(), Is.EqualTo(2));
+        }
+    }
+}
