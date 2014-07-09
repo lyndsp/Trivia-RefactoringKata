33c082e5d2ffb5d56217468a715470c8c977e32b
Lyndsay Prewer <lyndsayp@gmail.com>
2014-07-09 11:30:02 +0100

    Refactor to move tests.

--- Tests/GameHasUpToSixPlayers.cs
+++ Tests/GameHasUpToSixPlayers.cs
@@ -4,31 +4,32 @@ using System;
 namespace UglyTrivia
 {
     [TestFixture]
-    class FirstTests
+    class GameHasUpToSixPlayers
     {
+        Game game;
+
+        [SetUp]
+        public void CreateNewGame()
+        {
+            game = new Game();
+        }
+
         [Test]
         public void NewGameHasZeroPlayers()
         {
-            var game = new Game();
-
             Assert.That(game.howManyPlayers(), Is.EqualTo(0));
         }
 
         [Test]
         public void CanAddOnePlayer()
         {
-            var game = new Game();
-
             game.add("player");
-
             Assert.That(game.howManyPlayers(), Is.EqualTo(1));
         }
 
         [Test]
         public void CanAddMulitplePlayers()
         {
-            var game = new Game();
-
             game.add("player");
             game.add("player");
 
@@ -39,7 +40,6 @@ namespace UglyTrivia
         public void MaximumOfSixPlayers()
         {
             var players = new int[5];
-            var game = new Game();
 
             foreach (var p in players)
             {
---
+++ Tests/Roll.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UglyTrivia
+{
+    [TestFixture]
+    class Roll
+    {
+        Game game;
+
+        [SetUp]
+        public void CreateNewGame()
+        {
+            game = new Game();
+        }
+
+    }
+}
